4cc47d125f69362299a96e3131e92f4c5dec6cd4
Easoncoder <easonyehmobile1@gmail.com>
2023-01-14 22:40:45 +0800

    Lab10_1 Finished and Add Single Testbench asm

--- Hardware/Lab/load_test_data.sh
+++ Hardware/Lab/load_test_data.sh
@@ -19,11 +19,16 @@ then
     then
         cp ../../Emulator/inst.asm ./src/main/resource/inst.asm
         cp ../../Emulator/inst.hex ./src/main/resource/inst.hex
-	cp ../../Emulator/data.hex ./src/main/resource/data.hex
+	    cp ../../Emulator/data.hex ./src/main/resource/data.hex
+    elif [ "$1" = "-s" ]
+    then
+        cp ../../riscv-test/out/asm/rv32ui_SingleTest-$2.asm ./src/main/resource/inst.asm
+        cp ../../riscv-test/out/hex/text/rv32ui_SingleTest-$2.hex ./src/main/resource/inst.hex
+        cp ../../riscv-test/out/hex/data/rv32ui_SingleTest-$2.hex ./src/main/resource/data.hex
     else
-        cp ../../riscv-test/out/asm/rv32ui-$1.asm ./src/main/resource/inst.asm
-        cp ../../riscv-test/out/hex/text/rv32ui-$1.hex ./src/main/resource/inst.hex
-        cp ../../riscv-test/out/hex/data/rv32ui-$1.hex ./src/main/resource/data.hex
+        cp ../../riscv-test/out/asm/rv32ui_FullTest-$1.asm ./src/main/resource/inst.asm
+        cp ../../riscv-test/out/hex/text/rv32ui_FullTest-$1.hex ./src/main/resource/inst.hex
+        cp ../../riscv-test/out/hex/data/rv32ui_FullTest-$1.hex ./src/main/resource/data.hex
     fi
 else
     echo "[Error] usage should be: ./test_data.sh <which Test program> (Test1/Test2/Test3/Emulator/[inst code])"
